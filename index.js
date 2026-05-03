7f42c00a3acae9ef5f1756a730f0f87ed3431939
Tanishi17 <dubeytanishi14@gmail.com>
2026-05-03 16:15:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+// Home route
+app.get("/", (req, res) => {
+  res.send("🚀 Cloud Web App Backend is Running");
+});
+
+// API test route
+app.get("/api/status", (req, res) => {
+  res.json({
+    status: "OK",
+    message: "Server is working fine",
+    time: new Date()
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
